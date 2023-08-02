0749180a012d0714448dce1cafdaf05ef3f4d474
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2023-08-02 10:54:52 +0300

    Improved method names in benchmarks to be more understandable

--- benches/sharded_queue.rs
+++ benches/sharded_queue.rs
@@ -7,8 +7,10 @@ use std::thread::{available_parallelism, scope};
 use crossbeam_queue::SegQueue;
 use sharded_queue::ShardedQueue;
 
-fn crossbeam_queue_push_and_pop_concurrently(operation_count: usize) {
-    let max_concurrent_thread_count = available_parallelism().unwrap().get();
+fn crossbeam_queue_push_and_pop_concurrently(
+    operation_count: usize,
+    max_concurrent_thread_count: usize,
+) {
     let queue = SegQueue::new();
     scope(|scope| {
         for _ in 0..max_concurrent_thread_count {
@@ -22,8 +24,10 @@ fn crossbeam_queue_push_and_pop_concurrently(operation_count: usize) {
     });
 }
 
-fn sharded_queue_push_and_pop_concurrently(operation_count: usize) {
-    let max_concurrent_thread_count = available_parallelism().unwrap().get();
+fn sharded_queue_push_and_pop_concurrently(
+    operation_count: usize,
+    max_concurrent_thread_count: usize,
+) {
     let sharded_queue = ShardedQueue::<usize>::new(max_concurrent_thread_count);
 
     scope(|scope| {
@@ -38,8 +42,10 @@ fn sharded_queue_push_and_pop_concurrently(operation_count: usize) {
     });
 }
 
-fn queue_mutex_push_and_pop_concurrently(operation_count: usize) {
-    let max_concurrent_thread_count = available_parallelism().unwrap().get();
+fn queue_mutex_push_and_pop_concurrently(
+    operation_count: usize,
+    max_concurrent_thread_count: usize,
+) {
     let queue_mutex = Mutex::new(VecDeque::new());
 
     scope(|scope| {
@@ -63,7 +69,7 @@ fn queue_mutex_push_and_pop_concurrently(operation_count: usize) {
 
 fn criterion_benchmark(criterion: &mut Criterion) {
     struct BenchFnAndName {
-        bench_fn: fn(usize),
+        bench_fn: fn(usize, usize),
         bench_fn_name: String,
     }
 
@@ -84,13 +90,25 @@ fn criterion_benchmark(criterion: &mut Criterion) {
 
     let operation_count_list = [1e3 as usize, 1e4 as usize];
 
+    let max_concurrent_thread_count = available_parallelism().unwrap().get();
+
     for operation_count in operation_count_list {
+        let name_of_operation_count = String::from(name_of!(operation_count));
+
         for bench_fn_and_name in bench_fn_and_name_list.iter() {
             let (bench_fn, bench_fn_name) =
                 (bench_fn_and_name.bench_fn, &bench_fn_and_name.bench_fn_name);
-            criterion.bench_function(&format!("{bench_fn_name}_{operation_count}"), |bencher| {
-                bencher.iter(|| bench_fn(black_box(operation_count)))
-            });
+            criterion.bench_function(
+                &format!("{bench_fn_name}_{name_of_operation_count}={operation_count}_concurrent_thread_count={max_concurrent_thread_count}"),
+                |bencher| {
+                    bencher.iter(|| {
+                        bench_fn(
+                            black_box(operation_count),
+                            black_box(max_concurrent_thread_count),
+                        )
+                    })
+                },
+            );
         }
     }
 }
